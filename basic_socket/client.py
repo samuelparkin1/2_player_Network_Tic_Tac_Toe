8956160d5e15c8efdf7b4efa3be056424fe0b5be
samuel_parkin <samuelparkin1@gmail.com>
2021-09-23 13:55:56 +1000

    client and server now in classes

--- basic_socket/client.py
+++ basic_socket/client.py
@@ -1,44 +1,10 @@
 import socket, pickle
-class ProcessData:
-    def __init__(self, data= 'ABCDEFGHIJK'):
-        self.data = data
-    def __str__(self): return self.data
-
-HOST = socket.gethostbyname(socket.gethostname()) #automatically gets ip address of computer
-PORT = 50005
-# Create a socket connection.
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-
-while True:
-    # Create an instance of ProcessData() to send to server.
-    variable = input()
-    # Pickle the object and send it to the server
-    data_string = pickle.dumps(variable)
-
-    s.send(data_string)
-
-    #s.close()
-    print ('Data Sent to Server')
-    data = s.recv(40096)
-    data_variable = pickle.loads(data)
-    #conn.close()
-    print (data_variable)
-    print ('Data received from Server')
-
-
-
-
-
-
-'''
-import socket, pickle
 import game
 
 class Client():
     def __init__(self):
         self.server = socket.gethostbyname(socket.gethostname()) #automatically gets ip address of computer
-        self.port = 50006
+        self.port = 50053
         self.addr = (self.server, self.port)        
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect(self.addr)
@@ -56,4 +22,3 @@ class Client():
         
 client1 = Client()
 client1.comms()
-'''
--- basic_socket/server.py
+++ basic_socket/server.py
@@ -1,29 +1,38 @@
-class ProcessData:
-    def __init__(self, data= 'ABCDEFGHIJK'):
-        self.data = data
-    def __str__(self): return self.data
+import socket 
+import threading
+import pickle
 
-import socket, pickle
+class Server():
+    print ("Server online....")
 
-print ("Server is Listening.....")
-HOST = socket.gethostbyname(socket.gethostname()) #automatically gets ip address of computer
-PORT = 50005
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1)
-conn, addr = s.accept()
-print ('Connected by', addr)
+    def __init__ (self):
+        self.server_ip = socket.gethostbyname(socket.gethostname()) #automatically gets ip address of computer
+        self.port = 50053
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #Prevents changing port number 
+        self.server_socket.bind((self.server_ip, self.port))
+        
 
-while True:
-    data = conn.recv(4096)
-    data_variable = pickle.loads(data)
-    #conn.close()
-    print (data_variable)
-    print ('Data received from client')
 
-    variable = input()
-    # Pickle the object and send it to the server
-    data_string = pickle.dumps(variable)
-    conn.send(data_string)
-    #s.close()
-    print ('Data Sent to client')
+    def connect(self):
+        print ("Server listening for client....")
+        self.server_socket.listen(1)
+        conn, addr = self.server_socket.accept()
+        print ('Connected by', addr)
+
+        while True:
+            data = conn.recv(4096)
+            data_variable = pickle.loads(data)
+            #conn.close()
+            print (data_variable)
+            print ('Data received from client')
+
+            variable = input()
+            # Pickle the object and send it to the server
+            data_string = pickle.dumps(variable)
+            conn.send(data_string)
+            #s.close()
+            print ('Data Sent to client')
+
+server1 = Server()
+server1.connect() 
